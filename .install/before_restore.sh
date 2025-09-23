85f8b7924c703db710f96c58b47bd4b416857782
Matthew B. <berksapexlegends@gmail.com>
2025-09-23 12:43:23 -0500

    update

--- .install/before_restore.sh
+++
@@ -1,7 +0,0 @@
-# ------------------------------------------------------
-# Modify existing files before restore starts
-# ------------------------------------------------------
-
-if [ -d ~/dotfiles ] ;then
-    echo ":: Modify existing files"
-fi
